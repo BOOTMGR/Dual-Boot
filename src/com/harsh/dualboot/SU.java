67ce246b010ea359955b6e2f3e70538f1c957f62
Harsh Panchal <harsh.dude16@gmail.com>
2014-01-08 17:57:03 +0530

    Add licence

--- src/com/harsh/dualboot/SU.java
+++ src/com/harsh/dualboot/SU.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Harsh Panchal <panchal.harsh18@gmail.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package com.harsh.dualboot;
 
 import static com.harsh.dualboot.Constants.E;
