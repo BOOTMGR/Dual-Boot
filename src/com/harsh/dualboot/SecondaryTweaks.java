34b856f0748aaa0a5b429d040532e2283cf744c2
Harsh Panchal <harsh.dude16@gmail.com>
2014-02-12 07:47:58 +0530

    Remove app folder sharing - Its useless because GAPPS gives FC when they're upgraded on primary ROM. Use custom app share instead.

--- src/com/harsh/dualboot/SecondaryTweaks.java
+++ src/com/harsh/dualboot/SecondaryTweaks.java
@@ -7,7 +7,6 @@
  ******************************************************************************/
 package com.harsh.dualboot;
 
-import static com.harsh.dualboot.Constants.PRIMARY_DATA;
 import static com.harsh.dualboot.Constants.SECONDARY_CACHE;
 import static com.harsh.dualboot.Constants.SECONDARY_DATA;
 import android.app.Activity;
@@ -26,28 +25,12 @@ public class SecondaryTweaks extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.advance);
 		Button but_wipe = (Button) findViewById(R.id.but_wipe);
-		Button but_share = (Button) findViewById(R.id.but_share);
-		Button but_share_ret = (Button) findViewById(R.id.but_share_rev);
 		Button but_share_cust = (Button) findViewById(R.id.but_share_cust);
 		but_wipe.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				handlerRequest();
-			}
-		});
-		but_share.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				warnUser();
-			}
-		});
-		but_share_ret.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				mRevertShare();
 			}
 		});
 		but_share_cust.setOnClickListener(new OnClickListener() {
@@ -67,37 +50,6 @@ public class SecondaryTweaks extends Activity {
             public void onClick(DialogInterface dialog, int id) {
             	new SU(SecondaryTweaks.this).execute("rm -r " + SECONDARY_DATA);
             	new SU(SecondaryTweaks.this).execute("rm -r " + SECONDARY_CACHE);
-            	return;
-            }
-        });
-		builder.setNegativeButton(R.string.cancel, null);
-		builder.create().show();
-	}
-	
-	private void warnUser() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(R.string.share_dialog);
-		builder.setTitle(R.string.cleanup_warn_title);
-		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            	new SU(SecondaryTweaks.this).execute("rm -r " + SECONDARY_DATA + "/app");
-            	new SU(SecondaryTweaks.this).execute("ln -s " + PRIMARY_DATA + "/app " + SECONDARY_DATA + "/app");
-            	return;
-            }
-        });
-		builder.setNegativeButton(R.string.cancel, null);
-		builder.create().show();
-	}
-	
-	private void mRevertShare() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setMessage(R.string.share_rev_dialog);
-		builder.setTitle(R.string.cleanup_warn_title);
-		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            	new SU(SecondaryTweaks.this).execute("rm -r " + SECONDARY_DATA + "/app");
-            	new SU(SecondaryTweaks.this).execute("mkdir " + SECONDARY_DATA + "/app");
-            	new SU(SecondaryTweaks.this).execute("chmod 775 " + SECONDARY_DATA + "/app");
             	return;
             }
         });
